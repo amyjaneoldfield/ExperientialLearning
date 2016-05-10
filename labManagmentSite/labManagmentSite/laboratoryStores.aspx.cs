464287b2ec6526f939d34d19522e7f9dead653e5
1421049 <1421049@chester.ac.uk>
2016-05-10 11:57:20 +0100

    model changed

--- labManagmentSite/labManagmentSite/laboratoryStores.aspx.cs
+++ labManagmentSite/labManagmentSite/laboratoryStores.aspx.cs
@@ -5,18 +5,20 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+
 namespace labManagmentSite
 {
     public partial class laboratoryStores : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            var db = new db_1421049_LabManagementEntities();
+            var Categories = db.Lab_Stores.ToList();
+
+
 
         }
 
-        protected void rptrGases_DataBinding(object sender, EventArgs e)
-        {
-
-        }
+       
     }
 }
