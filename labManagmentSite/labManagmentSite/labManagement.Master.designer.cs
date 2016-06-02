e69e7571bd0938e311f21be6799c347ad3e2b1ae
1404159 <1404159@chester.ac.uk>
2016-06-02 11:57:50 +0100

    changes

--- labManagmentSite/labManagmentSite/labManagement.Master.designer.cs
+++ labManagmentSite/labManagmentSite/labManagement.Master.designer.cs
@@ -31,6 +31,15 @@ namespace labManagmentSite {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ContentPlaceHolder1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        
+        /// <summary>
         /// LoginStatus control.
         /// </summary>
         /// <remarks>
@@ -47,15 +56,6 @@ namespace labManagmentSite {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.LoginName LoginName;
-        
-        /// <summary>
-        /// ContentPlaceHolder1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
         /// LinkButton1 control.
