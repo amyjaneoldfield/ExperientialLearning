f5fdfd2ee61cd35b8ad258fe1712ceaeb0286b99
1409891 <1409891@chester.ac.uk>
2016-06-07 13:17:09 +0100

    validation fixed

--- labManagmentSite/labManagmentSite/admin/addEquipment.aspx.designer.cs
+++ labManagmentSite/labManagmentSite/admin/addEquipment.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace labManagmentSite.admin {
         protected global::System.Web.UI.WebControls.TextBox txtAddEquipName;
         
         /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        
+        /// <summary>
         /// lblEquipLocation control.
         /// </summary>
         /// <remarks>
@@ -49,6 +58,15 @@ namespace labManagmentSite.admin {
         protected global::System.Web.UI.WebControls.TextBox txtAddEquipLocation;
         
         /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
         /// lblAddEquipCustodian control.
         /// </summary>
         /// <remarks>
@@ -65,6 +83,15 @@ namespace labManagmentSite.admin {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtAddEquipCustodian;
+        
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
         /// Button2 control.
